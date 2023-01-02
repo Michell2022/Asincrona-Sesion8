ab2e21fed4d0a64cf1dbc166b0ca4a164944cc7e
Michell2022 <sistemas.angel04@gmail.com>
2023-01-02 01:45:03 -0500

    correciones finales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
@@ -8,17 +9,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   datos: any = [{
-    nombre: 'michell',
-    apellido:'quispe',
-    edad:'29'
+    nombre: 'demon',
+    apellido:'salvatore',
+    edad:22
   }];
+    
 
   registrarDato(nombre:string, apellido:string, edad:string){
 
-  parseInt(edad);
-
+  if(nombre.length && apellido.length && edad.length ){
     this.datos.push({
       nombre,apellido,edad
     });
+    
+  }else{
+    Swal.fire('[ ERROR ] DEBE LLENAR TODOS LOS CAMPOS')
+  }
+
+    
+    
   }
 }
